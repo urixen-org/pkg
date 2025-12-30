465bc49f6b2f74db4b1ee69d2ef5e303763a75f9
Nehxurai <158018273+xerinv0@users.noreply.github.com>
2025-12-30 21:50:11 +0600

    Delete scripts directory

--- scripts/sign.sh
+++
@@ -1,6 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cd "${SCRIPT_DIR}/../data"
-
-gpg --local-user DA6CD75EC2839680 -a -o Release.pgp --detach-sig Release
-gpg --local-user DA6CD75EC2839680 -a -o InRelease --clearsign Release
--- scripts/update.sh
+++
@@ -1,7 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cd "${SCRIPT_DIR}/../data"
-
-dpkg-scanpackages --multiversion . > Packages
-gzip -k -f Packages
-apt-ftparchive release . > Release
